83df676517b98be462481a21624b9e18fd8fdcad
Carlos Escobar <skalero01@gmail.com>
2025-03-19 00:50:01 -0600

    change design a little bit

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -11,21 +11,19 @@
                 <x-app-logo />
             </a>
 
-            <flux:navlist variant="outline">
-                <flux:navlist.group :heading="__('Platform')" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+            <flux:navlist variant="outline" class="relative">
+                <x-icon name="pencil-square" class="w-5 h-5 absolute hover:text-gray-200 cursor-pointer" style="top: 0; right: 0" />
+
+                <flux:navlist.group :heading="__('Ideas')" class="grid">
+                    <flux:navlist.item :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
-
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits" target="_blank">
-                {{ __('Documentation') }}
+                <flux:navlist.item icon="folder-git-2" href="https://github.com/weblabormx/ia-roi-chat" target="_blank">
+                    {{ __('Repository') }}
                 </flux:navlist.item>
             </flux:navlist>
 
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,6 +5,7 @@
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
+<wireui:scripts />
+<script src="//unpkg.com/alpinejs" defer></script>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
