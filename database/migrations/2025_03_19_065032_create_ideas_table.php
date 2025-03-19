1d951964b1ffe5eaa67b15f300c9c44bec11159b
Carlos Escobar <skalero01@gmail.com>
2025-03-19 01:25:08 -0600

    some fixes

--- database/migrations/2025_03_19_065032_create_ideas_table.php
+++ database/migrations/2025_03_19_065032_create_ideas_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('thread_id')->nullable();
+            $table->integer('user_id')->index();
             $table->timestamps();
         });
     }
