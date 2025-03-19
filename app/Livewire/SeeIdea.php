28426b5ecd6e83cbcfe63363f9fe2e07bd7607bb
Carlos Escobar <skalero01@gmail.com>
2025-03-19 01:43:31 -0600

    be able to add new messages

--- app/Livewire/SeeIdea.php
+++ app/Livewire/SeeIdea.php
@@ -7,13 +7,26 @@ use Livewire\Component;
 
 class SeeIdea extends Component
 {
-    public $idea;
+    public $idea, $message;
+    public $rules = [
+        'message' => 'required|min:4'
+    ];
 
     public function mount(Idea $idea)
     {
         $this->idea = $idea;
     }
-    
+
+    public function sendMessage()
+    {
+        $this->validate();
+        $this->idea->messages()->create([
+            'message' => $this->message,
+            'sent_by_user' => true
+        ]);
+        $this->message = '';
+    }
+
     public function render()
     {
         return view('livewire.see-idea');
